66dcc73befaef88d3de6d05b510aa424ce236e2f
jmic8397 <jmic8397@uni.sydney.edu.au>
2023-05-27 18:55:36 +1000

    fixed private repo functionality

--- images.py
+++ images.py
@@ -2,7 +2,7 @@
 import requests
 import json
 import csv
-#update with dataset name downloaded from epicollect
+import os
 
 MEDIAURL = 'https://five.epicollect.net/api/export/media/'
 
@@ -16,30 +16,70 @@ def load_variables(filename):
     SLUG = data.get("SLUG")
     APPID = data.get("APPID")
     IMAGE_FOLDER = data.get("IMAGE_FOLDER")
+    IMAGE_FOLDER = os.path.join(os.path.dirname(os.path.realpath('__file__')),IMAGE_FOLDER)
+    EXTRA_INFO = data.get("EXTRA_INFO")
+    CLIENTID = data.get("CLIENTID")
+    CLIENTSECRET = data.get("CLIENTSECRET")
+
     if(APPID == ''):
-        print("No App ID provided, assuming project has public status in Epicollect")
-    return FILENAME, NAMECOLS, IMGCOLS, SLUG, APPID, IMAGE_FOLDER
+        print("No App ID provided, assuming CLIENTID and CLIENTSECRET were provided or project has public status in Epicollect")
+    return FILENAME, NAMECOLS, IMGCOLS, SLUG, APPID, IMAGE_FOLDER, EXTRA_INFO, CLIENTID, CLIENTSECRET
     
 
-def get_image(imagestring, imagename,APPID,SLUG):
-    if not APPID == None:
+
+def get_image(imagestring, imagename,APPID,SLUG,IMAGE_FOLDER, CLIENTID, CLIENTSECRET):
+
+    #Private 
+    if not CLIENTID == "":
+
+        #Get APPID with POST request
+        if APPID == "":
+
+            params = {
+            'grant_type': 'client_credentials',
+            'client_id': CLIENTID,
+            'client_secret' : CLIENTSECRET,
+            }
+
+            response = requests.post('https://five.epicollect.net/api/oauth/token',params)
+            if(response.status_code == 404):
+                print("Unable to connect to private repo, check CLIENTID and CLIENTSECRET in data.json")
+                os.exit(0)
+            content = json.loads(response.content)
+
+            APPID = content.get('access_token')
+
         params = {
         'type': 'photo',
         'format': 'entry_original',
         'name' : imagename,
-        'access_token' : APPID
         }
+
+        headers = {
+            'Authorization': f'Bearer {APPID}',
+        }
+
+        response = requests.get(MEDIAURL+SLUG,params=params,headers=headers)
+
+    #Public
     else:
         params = {
         'type': 'photo',
         'format': 'entry_original',
         'name' : imagename
         }
+        response = requests.get(MEDIAURL+SLUG,params)
+        if(response.status_code == 404):
+            print("Failed to retrieve file: " + imagename)
+            return
+    
+
     print("Processing: "+ imagestring)
 
-    response = requests.get(MEDIAURL+SLUG,params)
+    
     content = response.content
-    with open(imagestring, 'wb') as f:
+    
+    with open(os.path.join(IMAGE_FOLDER,imagestring), 'wb') as f:
         f.write(content)
 
 #Scan through data and extract fields of interest
@@ -54,27 +94,25 @@ def getColIndicies(fields,NAMECOLS, IMGCOLS):
     return nameIndicies,imgIndicies
 
 def main():
-    FILENAME, NAMECOLS, IMGCOLS, SLUG, APPID, IMAGE_FOLDER = load_variables('data.json')
+    FILENAME, NAMECOLS, IMGCOLS, SLUG, APPID, IMAGE_FOLDER, EXTRA_INFO, CLIENTID, CLIENTSECRET = load_variables('data.json')
     NAMECOLS = [x for x in NAMECOLS.split(",") if x]
     IMGCOLS= [x for x in IMGCOLS.split(",") if x]
-    print(FILENAME, NAMECOLS, IMGCOLS, SLUG, APPID, IMAGE_FOLDER )
+    # print(FILENAME, NAMECOLS, IMGCOLS, SLUG, APPID, IMAGE_FOLDER, EXTRA_INFO )
 
     with open (FILENAME) as f:
         print("Opening " + FILENAME)
         data = [x for x in csv.reader(f, delimiter=',', quotechar='"')]
         headers = data[0]
-        print(headers)
 
     nameIndicies,imgIndicies = getColIndicies(headers,NAMECOLS, IMGCOLS)
 
-    #update to include user input cols instead of hardcoded
     for fields in data[1:]:    
-        stringDataList = [fields[i].strip().rstrip("\n") for i in nameIndicies]
+        stringDataList = [fields[i].strip().rstrip("\n") for i in nameIndicies] + [EXTRA_INFO]
         nameString = "_".join(stringDataList)
 
         for img in imgIndicies:
             if (fields[img].split(".")[-1] == "jpg"):
-                get_image(nameString + "_" + headers[img].strip().rstrip("\n") + "_" + fields[img],fields[img],APPID,SLUG,IMAGE_FOLDER)
+                get_image(nameString + "_" + headers[img].strip().rstrip("\n")+".jpg",fields[img],APPID,SLUG,IMAGE_FOLDER, CLIENTID, CLIENTSECRET)
             else:
                 print("No " + headers[img].rstrip("\n") + " img for " + nameString + " given: " + fields[img])
 
